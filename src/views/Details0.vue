8b60297c34a9514ec3cb5a69e7d2835822d15305
Ryan Jacobo <ryanjacobo@yahoo.com>
2020-12-19 14:20:49 -0800

    Created a 2nd Details.vue file
    Created a 2nd Details.vue file

--- src/views/Details0.vue
+++ src/views/Details0.vue
@@ -1,39 +1,48 @@
 <template>
   <div v-if="error">{{ error }}</div>
   <div v-if="post" class="post">
-    <h3>{{ post.title }}</h3>
-    <p class="pre">{{ post.body }}</p>
-    <button @click="handleClick" class="delete">delete post</button>
+    <h3>
+      {{ post.title }}
+    </h3>
+    <p class="body">{{ post.body }}</p>
+    <!-- <button>Delete</button> -->
+    <button class="delete" @click="deleteBlog">Delete</button>
   </div>
-  <div v-else>
-    <Spinner />
-  </div>
+  <div v-else><Spinner /></div>
 </template>
 
 <script>
 import getPost from "../composables/getPost";
+import Spinner from "../components/Spinner";
 import { useRoute, useRouter } from "vue-router";
 import { projectFirestore } from "../firebase/config";
-// component imports
-import Spinner from "../components/Spinner.vue";
+
 export default {
   props: ["id"],
   components: { Spinner },
   setup(props) {
+    // const { error, post, load } = getPost(props.id);
+
+    // utilizing useRoute instead of props.id. "props" argument can be ommited from setup().
     const route = useRoute();
     const { error, post, load } = getPost(route.params.id);
     const router = useRouter();
     load();
-    const handleClick = async () => {
+
+    const deleteBlog = async () => {
       await projectFirestore.collection("posts").doc(props.id).delete();
-      router.push("/");
+      router.push({ name: "Home" });
     };
-    return { error, post, handleClick };
+
+    return { error, post, deleteBlog };
   },
 };
 </script>
 
-<style scoped>
+<style>
+.tags a {
+  margin-right: 10px;
+}
 .post {
   max-width: 1200px;
   margin: 0 auto;
@@ -43,7 +52,7 @@ export default {
   line-height: 1.5em;
   margin-top: 40px;
 }
-.pre {
+.body {
   white-space: pre-wrap;
 }
 button.delete {
